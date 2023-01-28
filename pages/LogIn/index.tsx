9bf1ce5e3f99f3aefa1b50acce5749a9e0407963
raehan <raehan900809@gmail.com>
2023-01-28 21:16:36 +0900

    add: login page wiht get /user, post /user/login

--- pages/LogIn/index.tsx
+++ pages/LogIn/index.tsx
@@ -50,7 +50,7 @@ const LogIn = () => {
   }
 
   if (data) {
-    return <Navigate to="/workspace/sleact/channel/일반" />;
+    // return <Navigate to="/workspace/sleact/channel/일반" />;
   }
 
   // console.log(error, userData);
